4db134c49755853face898aa9514a4e1411c26b6
balajiblj036 <balajiblj036@gmail.com>
2020-05-16 20:44:12 +0530

    my changes

---
+++ aure_terraform.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.0.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "FAST" {
+name = "FASTresourcegroup1"
+location = "westus"
+}
+
